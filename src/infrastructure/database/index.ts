34c747d4064ff7907826443d883df4b6fac3307f
fres-sudo <francescocalicchio@hotmail.com>
2024-12-01 23:41:59 +0100

    [IMP] chagne migration strategy

--- src/infrastructure/database/index.ts
+++ src/infrastructure/database/index.ts
@@ -2,7 +2,14 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import { config } from "../../common/config";
 import * as schema from "./tables";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
 
 export const client = postgres(config.postgres.url, { max: 10 });
 
 export const db = drizzle(client, { schema });
+
+if (config.isProduction) {
+  void migrate(db, {
+    migrationsFolder: "./src/server/api/infrastructure/database/migrations",
+  });
+}
