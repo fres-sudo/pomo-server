ab714954a5b35fdc04982ffaa2bec3367c18c413
fres-sudo <francescocalicchio@hotmail.com>
2024-12-21 18:42:50 +0100

    [FIX] migration out directory

--- src/infrastructure/database/migrate.ts
+++ src/infrastructure/database/migrate.ts
@@ -16,7 +16,7 @@ async function runMigration() {
   const db = drizzle(client);
   try {
     await migrate(db, {
-      migrationsFolder: "./src/server/api/infrastructure/database/migrations",
+      migrationsFolder: "./migrations",
     });
   } catch (error) {
     logger.error(`Migration failed 🚨: ${error}`);
