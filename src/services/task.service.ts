79cb0c608a081cbaf0fa0f9a6696db8ab87b2d79
fres-sudo <francescocalicchio@hotmail.com>
2025-01-26 21:49:43 +0100

    [FIX] task: fetch strategy

--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -4,76 +4,76 @@ import { CreateTaskDto } from "../dtos/task.dto";
 import { ProjectRepository } from "../repositories/project.repository";
 
 enum CalendarFormat {
-  month,
-  twoWeeks,
-  week,
+	month,
+	twoWeeks,
+	week,
 }
 
 export function parseCalendarFormat(format: string) {
-  if (format === "twoWeeks") {
-    return CalendarFormat.twoWeeks;
-  }
-  if (format === "week") {
-    return CalendarFormat.week;
-  }
-  return CalendarFormat.month;
+	if (format === "twoWeeks") {
+		return CalendarFormat.twoWeeks;
+	}
+	if (format === "week") {
+		return CalendarFormat.week;
+	}
+	return CalendarFormat.month;
 }
 
 @injectable()
 export class TaskService {
-  constructor(
-    @inject(TaskRepository)
-    private readonly taskRepository: TaskRepository,
-    @inject(ProjectRepository)
-    private readonly projectRepository: ProjectRepository,
-  ) {}
+	constructor(
+		@inject(TaskRepository)
+		private readonly taskRepository: TaskRepository,
+		@inject(ProjectRepository)
+		private readonly projectRepository: ProjectRepository
+	) {}
 
-  async getAllTasks() {
-    return this.taskRepository.findAll();
-  }
+	async getAllTasks() {
+		return this.taskRepository.findAll();
+	}
 
-  async getTasksByUser(id: string) {
-    return this.taskRepository.findAllByUser(id);
-  }
+	async getTasksByUser(id: string) {
+		return await this.taskRepository.findAllByUser(id);
+	}
 
-  async getTaskByFormat(date: Date, userId: string, format: CalendarFormat) {
-    if (format === CalendarFormat.month) {
-      return this.taskRepository.findAllByMonth(date, userId);
-    }
-    if (format === CalendarFormat.twoWeeks) {
-      return this.taskRepository.findAllByTwoWeeks(date, userId);
-    }
-    if (format === CalendarFormat.week) {
-      return this.taskRepository.findAllByWeek(date, userId);
-    }
-    return this.taskRepository.findAllByMonth(date, userId);
-  }
+	async getTaskByFormat(date: Date, userId: string, format: CalendarFormat) {
+		if (format === CalendarFormat.month) {
+			return await this.taskRepository.findAllByMonth(date, userId);
+		}
+		if (format === CalendarFormat.twoWeeks) {
+			return await this.taskRepository.findAllByTwoWeeks(date, userId);
+		}
+		if (format === CalendarFormat.week) {
+			return await this.taskRepository.findAllByWeek(date, userId);
+		}
+		return await this.taskRepository.findAllByMonth(date, userId);
+	}
 
-  async getAllByProject(projectId: string) {
-    return this.taskRepository.findAllByProject(projectId);
-  }
+	async getAllByProject(projectId: string) {
+		return await this.taskRepository.findAllByProject(projectId);
+	}
 
-  async createTask(data: CreateTaskDto) {
-    const task = await this.taskRepository.create(data);
-    this.updateProjectStatus(data.projectId);
-    return task;
-  }
+	async createTask(data: CreateTaskDto) {
+		const task = await this.taskRepository.create(data);
+		this.updateProjectStatus(data.projectId);
+		return task;
+	}
 
-  async updateTask(id: string, data: UpdateTaskDto) {
-    const task = this.taskRepository.update(id, data);
-    this.updateProjectStatus(data.projectId);
-    return task;
-  }
+	async updateTask(id: string, data: UpdateTaskDto) {
+		const task = this.taskRepository.update(id, data);
+		this.updateProjectStatus(data.projectId);
+		return task;
+	}
 
-  async deleteTask(id: string) {
-    const task = await this.taskRepository.delete(id);
-    this.updateProjectStatus(task.projectId);
-    return task;
-  }
+	async deleteTask(id: string) {
+		const task = await this.taskRepository.delete(id);
+		this.updateProjectStatus(task.projectId);
+		return task;
+	}
 
-  updateProjectStatus(projectId: string | null | undefined) {
-    if (projectId) {
-      this.projectRepository.updateProjectStatus(projectId);
-    }
-  }
+	updateProjectStatus(projectId: string | null | undefined) {
+		if (projectId) {
+			this.projectRepository.updateProjectStatus(projectId);
+		}
+	}
 }
