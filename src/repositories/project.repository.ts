743a5fd57d2721ace02a3b747f9418a0cae06d19
fres-sudo <francescocalicchio@hotmail.com>
2025-01-26 23:33:46 +0100

    [FIX] project: remove task overdue dep on proj status

--- src/repositories/project.repository.ts
+++ src/repositories/project.repository.ts
@@ -10,97 +10,91 @@ export type UpdateProjectDto = Partial<CreateProjectDto>;
 
 @injectable()
 export class ProjectRepository implements Repository {
-  constructor(@inject(DatabaseProvider) private db: DatabaseProvider) {}
+	constructor(@inject(DatabaseProvider) private db: DatabaseProvider) {}
 
-  async findAll(): Promise<Project[]> {
-    return this.db.query.projectsTable.findMany({
-      with: {
-        tasks: true,
-      },
-    });
-  }
+	async findAll(): Promise<Project[]> {
+		return this.db.query.projectsTable.findMany({
+			with: {
+				tasks: true,
+			},
+		});
+	}
 
-  async findOneById(id: string) {
-    return this.db.query.projectsTable.findFirst({
-      where: eq(projectsTable.id, id),
-      with: {
-        tasks: true,
-      },
-    });
-  }
+	async findOneById(id: string) {
+		return this.db.query.projectsTable.findFirst({
+			where: eq(projectsTable.id, id),
+			with: {
+				tasks: true,
+			},
+		});
+	}
 
-  async findAllByUser(userId: string): Promise<Project[]> {
-    return this.db.query.projectsTable.findMany({
-      where: and(eq(projectsTable.userId, userId)),
-      with: {
-        tasks: true,
-      },
-    });
-  }
+	async findAllByUser(userId: string): Promise<Project[]> {
+		return this.db.query.projectsTable.findMany({
+			where: and(eq(projectsTable.userId, userId)),
+			with: {
+				tasks: true,
+			},
+		});
+	}
 
-  async create(data: CreateProjectDto) {
-    return this.db
-      .insert(projectsTable)
-      .values(data)
-      .returning()
-      .then(takeFirstOrThrow);
-  }
+	async create(data: CreateProjectDto) {
+		return this.db
+			.insert(projectsTable)
+			.values(data)
+			.returning()
+			.then(takeFirstOrThrow);
+	}
 
-  async update(id: string, data: UpdateProjectDto) {
-    return this.db
-      .update(projectsTable)
-      .set(data)
-      .where(eq(projectsTable.id, id))
-      .returning()
-      .then(takeFirstOrThrow);
-  }
+	async update(id: string, data: UpdateProjectDto) {
+		return this.db
+			.update(projectsTable)
+			.set(data)
+			.where(eq(projectsTable.id, id))
+			.returning()
+			.then(takeFirstOrThrow);
+	}
 
-  async delete(id: string) {
-    return this.db
-      .delete(projectsTable)
-      .where(eq(projectsTable.id, id))
-      .returning()
-      .then(takeFirstOrThrow);
-  }
-  async updateProjectStatus(projectId: string) {
-    const project = await this.findOneById(projectId);
-    const status = project?.status;
-    let newStatus = status;
-    const currentDate = new Date();
+	async delete(id: string) {
+		return this.db
+			.delete(projectsTable)
+			.where(eq(projectsTable.id, id))
+			.returning()
+			.then(takeFirstOrThrow);
+	}
+	async updateProjectStatus(projectId: string) {
+		const project = await this.findOneById(projectId);
+		const status = project?.status;
+		let newStatus = status;
+		const currentDate = new Date();
 
-    const anyTaskOverdue = project?.tasks.some(
-      (task) =>
-        task.pomodoro !== task.pomodoroCompleted &&
-        new Date(task.dueDate) < currentDate,
-    );
-    const allTasksCompleted = project?.tasks.every(
-      (task) => task.pomodoro === task.pomodoroCompleted,
-    );
+		const allTasksCompleted = project?.tasks.every(
+			(task) => task.pomodoro === task.pomodoroCompleted
+		);
 
-    const isExpired =
-      new Date(project?.endDate ?? new Date()) <= currentDate || anyTaskOverdue;
+		const isExpired = new Date(project?.endDate ?? new Date()) <= currentDate;
 
-    if (isExpired) {
-      newStatus = "expired";
-    } else if (allTasksCompleted) {
-      newStatus = "completed";
-    } else if (status === "archived") {
-      newStatus = "archived";
-    } else {
-      newStatus = "progress";
-    }
+		if (isExpired) {
+			newStatus = "expired";
+		} else if (allTasksCompleted) {
+			newStatus = "completed";
+		} else if (status === "archived") {
+			newStatus = "archived";
+		} else {
+			newStatus = "progress";
+		}
 
-    if (newStatus !== status) {
-      await this.db
-        .update(projectsTable)
-        .set({ status: newStatus })
-        .where(eq(projectsTable.id, projectId))
-        .returning()
-        .then(takeFirstOrThrow);
-    }
-  }
+		if (newStatus !== status) {
+			await this.db
+				.update(projectsTable)
+				.set({ status: newStatus })
+				.where(eq(projectsTable.id, projectId))
+				.returning()
+				.then(takeFirstOrThrow);
+		}
+	}
 
-  trxHost(trx: DatabaseProvider) {
-    return new ProjectRepository(trx);
-  }
+	trxHost(trx: DatabaseProvider) {
+		return new ProjectRepository(trx);
+	}
 }
